4bf151324776717e135ad79c5a34d94477ef16a9
Ramiro Estrada Garcia <ramiroestradag@gmail.com>
2023-09-28 11:00:05 -0600

    feat: :wrench: Tailwind, PrimeNG config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
